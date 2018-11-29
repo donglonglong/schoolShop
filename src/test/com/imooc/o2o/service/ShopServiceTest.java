34e8ffd98367e8524178006a4a1f843b79d69bbb
donglonglong <790347194@qq.com>
2018-11-29 17:28:17 +0800

    添加店铺信息

---
+++ src/test/com/imooc/o2o/service/ShopServiceTest.java
@@ -0,0 +1,42 @@
+package com.imooc.o2o.service;
+
+import com.imooc.o2o.BaseTest;
+import com.imooc.o2o.dto.ShopExecution;
+import com.imooc.o2o.entity.Area;
+import com.imooc.o2o.entity.PersonInfo;
+import com.imooc.o2o.entity.Shop;
+import com.imooc.o2o.entity.ShopCategory;
+import com.imooc.o2o.enums.ShopStateEnum;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import java.util.Date;
+
+public class ShopServiceTest extends BaseTest {
+    @Autowired
+    private ShopService shopService;
+
+    @Test
+    public void  testAddShop(){
+        Shop shop = new Shop();
+        PersonInfo owner = new PersonInfo();
+        Area area = new Area();
+        ShopCategory shopCategory = new ShopCategory();
+        owner.setUserId(8L);
+        area.setAreaId(3);
+        shopCategory.setShopCategoryId(30L);
+        shop.setArea(area);
+        shop.setShopCategory(shopCategory);
+        shop.setOwner(owner);
+        shop.setShopName("测试的店铺");
+        shop.setShopDesc("test");
+        shop.setShopAddr("test");
+        shop.setPhone("test");
+        shop.setPriority(90);
+        shop.setCreateTime(new Date());
+        shop.setEnableStatus(ShopStateEnum.CHECK.getState());
+        shop.setAdvice("审核中");
+
+    }
+}
