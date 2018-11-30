228b41c9cd73e91f4c04100995a5515b48a9a1cc
donglonglong <790347194@qq.com>
2018-11-30 11:30:21 +0800

    更新logback

--- src/main/java/com/imooc/o2o/util/ImageUtil.java
+++ src/main/java/com/imooc/o2o/util/ImageUtil.java
@@ -2,14 +2,29 @@ package com.imooc.o2o.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
+
 
 import net.coobird.thumbnailator.Thumbnails;
 
+import net.coobird.thumbnailator.geometry.Positions;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import javax.imageio.ImageIO;
+
+
+
 public class ImageUtil {
+
+
+   private static String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath(); //获得当前执行的线程
+   private static  final SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+   private static  final Random r = new Random();//定义随机
+
     public static String generateThumbnail(CommonsMultipartFile thumbnail, String targetAddr) {
         String realFileName = FileUtil.getRandomFileName();
         String extension = getFileExtension(thumbnail);
@@ -23,6 +38,7 @@ public class ImageUtil {
         }
         return relativeAddr;
     }
+
 
     public static String generateNormalImg(CommonsMultipartFile thumbnail, String targetAddr) {
         String realFileName = FileUtil.getRandomFileName();
@@ -60,9 +76,9 @@ public class ImageUtil {
         return relativeAddrList;
     }
 
-    /***
-     * 创建目标路径所涉及到的目录，/home/work/xiangze/xxx.jpg
-     * 那么home work xiangze 这三个文件夹都得自动创建
+
+    /**
+     * 创建目标路径所涉及的目录，即 home work  xiangze 这三个文件夹自动创建
      * @param targetAddr
      */
     private static void makeDirPath(String targetAddr) {
@@ -73,6 +89,8 @@ public class ImageUtil {
         }
     }
 
+
+
     /**
      * 获取输入文件流的扩展名
      * @param cFile
@@ -81,5 +99,18 @@ public class ImageUtil {
     private static String getFileExtension(CommonsMultipartFile cFile) {
         String originalFileName = cFile.getOriginalFilename();
         return originalFileName.substring(originalFileName.lastIndexOf("."));
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        /**
+         * 改变小黄人图片大小，加上水印，图片进行压缩
+         */
+        String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath(); //获得当前执行的线程
+        Thumbnails.of(new File("D:/JavaTest/o2o/src/main/webapp/resources/images/xiaohuangren.jpg"))
+                .size(200,200 ).watermark(Positions.BOTTOM_RIGHT,
+                ImageIO.read(new File(basePath+"/watermark.jpg")),0.9f).outputQuality(0.8f)
+                .toFile("D:/JavaTest/o2o/src/main/webapp/resources/images/xiaohuangrenNew.jpg");
+
     }
 }
